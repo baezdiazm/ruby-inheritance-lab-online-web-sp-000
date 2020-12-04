044200aec924586f32f4ee76eb0ba48bca2ed08b
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-04 05:00:37 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User
-  attr_accessor :name, :last_name
+  attr_accessor :first_name, :last_name
 
-  def initialize(name, last_name)
+  def initialize(first_name, last_name)
     @name = name
     @last_name = last_name
   end
